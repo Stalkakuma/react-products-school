ab18a4545d61c50babc3b49e1b9c2c393d979ff8
Stalkakuma <stalkakuma@gmail.com>
2024-10-09 17:20:11 +0300

    added User context provider for app with login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { NavBar } from './components/NavBar';
 import { ProductList } from './components/ProductList';
+import { UserProvider } from './store/UserContext';
 import { GlobalStyle, StyledApp } from './styes/GlobalStyles';
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <GlobalStyle />
       <StyledApp>
         <NavBar />
         <ProductList />
       </StyledApp>
-    </>
+    </UserProvider>
   );
 }
 
