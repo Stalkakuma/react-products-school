9fdaa5c812750f559864552c2204f1b3bcfaa77b
Stalkakuma <stalkakuma@gmail.com>
2024-10-09 18:22:11 +0300

    Moved modal to ProductList parent component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,72 +1,24 @@
 import { calculatePrice } from '../scripts/conversions';
-import { StyledButton } from '../styes/GlobalStyles';
-import {
-  StyledCardInfo,
-  StyledImage,
-  StyledCard,
-  StyledImageBox,
-  StyledDiscountBox,
-  StyledModalOverlay,
-  StyledModalCard,
-  StyledModalButtonBox,
-  StyledToast,
-  StyledModalImageBox,
-} from '../styes/ProductSection';
-import { useState } from 'react';
+import { StyledCardInfo, StyledImage, StyledCard, StyledImageBox, StyledDiscountBox } from '../styes/ProductSection';
 
-export const Product = ({ props }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [showToast, setShowToast] = useState(false);
-  const changeModal = () => setIsModalOpen(!isModalOpen);
-  const triggerToast = () => {
-    setShowToast(true);
-
-    setTimeout(() => {
-      setShowToast(false);
-    }, 3000);
-  };
+export const Product = ({ product, handleShow }) => {
+  const { title, image, price, text, category, quantity, discount } = product;
 
   return (
     <>
-      {isModalOpen && (
-        <StyledModalOverlay>
-          <StyledModalCard>
-            <StyledModalImageBox>
-              <StyledImage src={props.image} alt={props.title} />
-              {showToast && (
-                <StyledToast>
-                  <p>{props.title} added to cart!</p>
-                  <p>Thank you!</p>
-                </StyledToast>
-              )}
-            </StyledModalImageBox>
-            <StyledCardInfo>
-              <h2>{props.title}</h2>
-              <span>
-                {props.quantity}/{props.quantity} units available
-              </span>
-              <p>{props.text}</p>
-            </StyledCardInfo>
-            <StyledModalButtonBox>
-              <StyledButton onClick={() => triggerToast()}>Add to Cart</StyledButton>
-              <StyledButton onClick={() => changeModal()}>Close</StyledButton>
-            </StyledModalButtonBox>
-          </StyledModalCard>
-        </StyledModalOverlay>
-      )}
-      <StyledCard onClick={() => changeModal()}>
-        {props.discount === 0 || !props.discount ? (
+      <StyledCard onClick={() => handleShow(product)}>
+        {discount === 0 || !discount ? (
           <StyledDiscountBox>NEW!</StyledDiscountBox>
         ) : (
           <StyledDiscountBox>ON SALE!</StyledDiscountBox>
         )}
         <StyledImageBox>
-          <StyledImage src={props.image} alt={props.title} />
+          <StyledImage src={image} alt={title} />
         </StyledImageBox>
         <StyledCardInfo>
-          <h2>{props.title}</h2>
-          <span>${props.discount ? calculatePrice(parseInt(props.price), props.discount) : props.price}</span>
-          <p>{props.text}</p>
+          <h2>{title}</h2>
+          <span>${discount ? calculatePrice(parseInt(price), discount) : price}</span>
+          <p>{text}</p>
         </StyledCardInfo>
       </StyledCard>
     </>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,20 +1,75 @@
 import { Product } from './Product';
 import { shirtsList } from '../assets/data-imports';
-import { StyledGrid, StyledUserGreeting } from '../styes/ProductSection';
-import React, { useContext } from 'react';
+import {
+  StyledGrid,
+  StyledUserGreeting,
+  StyledModalOverlay,
+  StyledModalImageBox,
+  StyledModalCard,
+  StyledImage,
+  StyledCardInfo,
+  StyledModalButtonBox,
+  StyledToast,
+} from '../styes/ProductSection';
+import { StyledButton } from '../styes/GlobalStyles';
+import React, { useContext, useState } from 'react';
 import { UserContext } from '../store/UserContext';
 
 export const ProductList = () => {
   const { user } = useContext(UserContext);
+  const [modalProduct, setModalProduct] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+  const changeModal = () => setIsModalOpen(false);
+
+  const handleShow = (product) => {
+    setModalProduct(product);
+    setIsModalOpen(true);
+  };
+
+  const triggerToast = () => {
+    setShowToast(true);
+
+    setTimeout(() => {
+      setShowToast(false);
+    }, 3000);
+  };
 
   return (
     <>
       <StyledUserGreeting>{user ? <h2>Welcome {user.name}</h2> : <h2>You are not logged in!</h2>}</StyledUserGreeting>
       <StyledGrid>
-        {shirtsList.map((shirt) => (
-          <Product key={shirt.title} props={shirt} />
+        {shirtsList.map((product) => (
+          <Product key={product.title} product={product} handleShow={handleShow} />
         ))}
       </StyledGrid>
+
+      {isModalOpen && (
+        <StyledModalOverlay>
+          <StyledModalCard>
+            <StyledModalImageBox>
+              <StyledImage src={modalProduct?.image} alt={modalProduct?.title} />
+              {showToast && (
+                <StyledToast>
+                  <p>{modalProduct?.title} added to cart!</p>
+                  <p>Thank you!</p>
+                </StyledToast>
+              )}
+            </StyledModalImageBox>
+            <StyledCardInfo>
+              <h2>{modalProduct?.title}</h2>
+              <span>
+                {modalProduct?.quantity}/{modalProduct?.quantity} units available
+              </span>
+              <p>{modalProduct?.text}</p>
+            </StyledCardInfo>
+            <StyledModalButtonBox>
+              <StyledButton onClick={() => triggerToast()}>Add to Cart</StyledButton>
+              <StyledButton onClick={() => changeModal()}>Close</StyledButton>
+            </StyledModalButtonBox>
+          </StyledModalCard>
+        </StyledModalOverlay>
+      )}
     </>
   );
 };
